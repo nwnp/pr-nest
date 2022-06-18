25d2b87d3beabbcaeb8b1942e6dd899b87dc0aac
nwnp <jjw_0418@naver.com>
2022-06-18 21:28:21 +0900

    bug fix: Edit dto bug

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
